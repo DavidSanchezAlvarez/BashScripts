7e607abc5860282bf41a8ab7048c1ab1690c8761
David Sánchez Álvarez <48554043+DavidSanchezAlvarez@users.noreply.github.com>
2019-04-22 17:48:11 +0200

    Create CopyLastFiles.sh
    First commit.

---
+++ CopyLastFiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#Script to copy backups to an external location using SCP
+#David Sanchez Alvarez April/2019
+
+# Databases to copy
+declare -a "databases=( $( < schemas.txt ) )"
+
+#Backups folder
+#ATTENTION: Including / at the end
+backup_dir=/mnt/bckp_dir/
+dir_destiny=/mnt/HD/HD_a2/MySQL/
+ip_destiny=172.23.255.208
+us_destiny=root
+
+#Archivo de log                                                                                                       │
+logfile=./weekly-copy.log
+
+#Check every defined schema
+for database in "${databases[@]}"                                                                                     │
+do
+  #List most recent file of every schema
+  fn=$(ls -t "$backup_dir$database"* | head -n1)
+  # Continue if we obtained a result
+  if  [ ! -z "$fn" ]
+  then
+    echo "Copying $fn to $dir_destino"
+    echo "Copying $fn to $dir_destino" >> $logfile
+    # File copy. We use sshpass to read credentiales a bit safely from the external file
+    sshpass -f ".credencials" scp -r "$fn" "$us_destiny"@"$ip_destiny":"$dir_destiny"
+  fi
+done
