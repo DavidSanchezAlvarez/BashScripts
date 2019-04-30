61f6151685d7ec922b070b465bc176e77d683572
David Sánchez Álvarez <48554043+DavidSanchezAlvarez@users.noreply.github.com>
2019-04-30 11:18:58 +0200

    Update CopyLastFiles.sh
    Added date to log lines.

--- CopyLastFiles.sh
+++ CopyLastFiles.sh
@@ -26,7 +26,7 @@ do
   # Continue if we obtained a result
   if  [ ! -z "$fn" ]
   then
-    echo "Copying $fn to $dir_destino"
+    echo $(date +%d-%m-%Y/%T)" - Copying $fn to $dir_destino"
     echo "Copying $fn to $dir_destino" >> $logfile
     # File copy. We use sshpass to read credentiales a bit safely from the external file
     sshpass -f ".credencials" scp -r "$fn" "$us_destiny"@"$ip_destiny":"$dir_destiny"
